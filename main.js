08d9f2445f25ecdd04424ea7f489a16ee6cf1f19
gokrusk <nigelljama@gmail.com>
2023-01-07 01:11:29 -0500

    header, footer

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+//header
+let a = document.querySelector(".head1")
+let b = document.querySelector(".head2")
+let c = document.querySelector(".head3")
+let header = [a,b,c]
+
+header.forEach(element => {
+    if(element != null)
+    {
+        element.style.position = "fixed"
+        element.style.top = "0"
+        element.style.width = "100%"
+        element.style.opacity = "0.9"
+    }
+});
+
+//footer
 let footer = document.querySelector(".footer-section")
 let footerText = document.querySelector(".footer-text")
 footer.style.background = "#acabab"
